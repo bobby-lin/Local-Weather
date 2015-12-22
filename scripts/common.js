c16bb6872771472e9f39263a3f641a20420603ec
Bobby Lin <bobbylinjh@gmail.com>
2015-12-22 22:16:11 +0800

    Added latitude and longitude var in common.js

--- scripts/common.js
+++ scripts/common.js
@@ -3,6 +3,15 @@
  *     Author: Bobby Lin
  */
 
+var latitude = 0;
+var longitude = 0;
+
 $(document).ready(function() {
-    
-})
+   if(navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition(function(position) {
+           latitude = position.coords.latitude;
+           longitude = position.coords.longitude;
+           console.log("Lat: " + latitude + " Long: " + longitude ); 
+       });
+   }
+});
