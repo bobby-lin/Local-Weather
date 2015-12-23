8c5c5c15eb2f60b1474783c16de6b3b38ca9811e
Bobby Lin <bobbylinjh@gmail.com>
2015-12-23 12:01:17 +0800

    Removed methods in common.js

--- scripts/common.js
+++ scripts/common.js
@@ -3,44 +3,6 @@
  *     Author: Bobby Lin
  */
 
-var latitude = 0;
-var longitude = 0;
-var openWeather = "";
-
-function setOpenWeatherAPI() {
-    $.getJSON("config.json", function(data) {
-        var keys = Object.keys(data);
-        var arr = data[keys[0]];
-        arr.forEach(function(val){
-            var key = Object.keys(val);
-            if(key[0] === "OpenWeatherAPI") {
-                openWeather = val[key[0]];
-            }
-        });
-    });
-}
-
-function getWeatherJSON() {
-    var url = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude + "&lon=" + longitude;
-    url += "&APPID=" + openWeather;
-    $.getJSON(url,function(json) {
-        console.log(json);
-    })
-}
-
-function setCoordinates() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            latitude = position.coords.latitude;
-            longitude = position.coords.longitude;
-            console.log("Lat: " + latitude + " Long: " + longitude);
-
-        });
-    }
-}
-
 $(document).ready(function() {
-    setOpenWeatherAPI();
-    setCoordinates();
-    getWeatherJSON();
+    
 });
